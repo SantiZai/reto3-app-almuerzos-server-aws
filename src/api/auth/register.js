d4972d2b07085aae9f2d5a5bd458722210976aea
SantiZai <santyagozaidan@gmail.com>
2024-04-29 15:35:59 -0300

    new column for the email

--- src/api/auth/register.js
+++ src/api/auth/register.js
@@ -1,34 +1,30 @@
 const { v4 } = require("uuid");
 
-const {
-  DynamoDBDocument,
-} = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
 
-const {
-  DynamoDB,
-} = require("@aws-sdk/client-dynamodb");
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 
 module.exports.register = async (event) => {
   try {
     const dynamoDb = DynamoDBDocument.from(new DynamoDB());
 
-    const { fullname, identifier, position } = JSON.parse(event.body);
+    const { fullname, email, identifier, position } = JSON.parse(event.body);
     const id = v4();
-    const date = new Date
-    const createdAt = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
+    const date = new Date();
+    const createdAt = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
     const newEmployee = {
       id,
       createdAt,
       fullname: fullname.toLowerCase(),
+      email: email.toLowerCase(),
       identifier: String(identifier),
       position: position.toLowerCase(),
     };
 
-    await dynamoDb
-      .put({
-        TableName: "Employees",
-        Item: newEmployee,
-      });
+    await dynamoDb.put({
+      TableName: "Employees",
+      Item: newEmployee,
+    });
 
     return {
       statusCode: 201,
